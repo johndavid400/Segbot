bb2008a9e6179ca804d3f0b778baae49cd1410b0
JD <johndavid400@gmail.com>
2013-08-24 18:07:19 -0500

    updated

--- radio_shack_sensors/radio_shack_sensors.ino
+++ radio_shack_sensors/radio_shack_sensors.ino
@@ -4,6 +4,9 @@
 
 // This code is a work-in-progress and will be labeled as version 4 (v4), once completed
 
+boolean read_x = false;
+boolean read_y = true;
+
 // declare output pins for x and y accelerometer
 int x_accel = 8;
 int y_accel = 7;
@@ -37,23 +40,31 @@ void loop(){
 }
 
 void read_accel(){
-  // read the x axis of the accelerometer
-  x_raw = pulseIn(x_accel, HIGH);
-  x_adj = map(x_raw, accel_low, accel_high, -90, 90);
-  // read the y axis of the accelerometer
-  y_raw = pulseIn(y_accel, HIGH);
-  y_adj = map(y_raw, accel_low, accel_high, -90, 90);
+  if(read_x){
+    // read the x axis of the accelerometer
+    x_raw = pulseIn(x_accel, HIGH);
+    x_adj = map(x_raw, accel_low, accel_high, -90, 90);
+  }
+  if(read_y){
+    // read the y axis of the accelerometer
+    y_raw = pulseIn(y_accel, HIGH);
+    y_adj = map(y_raw, accel_low, accel_high, -90, 90);
+  }
 }
 
 void print_values(){
-  // print x values
-  Serial.print("X: ");
-  Serial.print(x_raw);
-  Serial.print("     "); 
-  // print y values
-  Serial.print("Y: ");
-  Serial.print(y_raw);
-  Serial.print("     ");
+  if(read_x){
+    // print x values
+    Serial.print("X: ");
+    Serial.print(x_adj);
+    Serial.print("     "); 
+  }
+  if(read_y){
+    // print y values
+    Serial.print("Y: ");
+    Serial.print(y_adj);
+    Serial.print("     ");
+  }
   // end of line
   Serial.println("");
 }
